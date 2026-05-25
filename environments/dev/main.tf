faedf7249f44784c741d1f1aee6ac094dab3a1b1
Atharva Pophali <atharvapophali3@gmail.com>
2026-05-25 17:52:28 +0530

    Added storage and database infrastructure modules

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,3 +10,34 @@ module "network" {
   private_subnet_1_cidr = "10.0.3.0/24"
   private_subnet_2_cidr = "10.0.4.0/24"
 }
+
+module "compute" {
+  source = "../../modules/compute"
+
+  ami_id        = "ami-07a00cf47dbbc844c"
+  instance_type = "t3.micro"
+
+  subnet_id = module.network.public_subnet_1_id
+  vpc_id    = module.network.vpc_id
+
+  key_name = "enterprise-key"
+}
+
+module "storage" {
+  source = "../../modules/storage"
+
+  bucket_name = "atharva-enterprise-storage-bucket"
+}
+
+module "database" {
+  source = "../../modules/database"
+
+  db_name     = "enterprise_db"
+  db_username = "postgres"
+  db_password = "StrongPassword123!"
+
+  private_subnet_1_id = module.network.private_subnet_1_id
+  private_subnet_2_id = module.network.private_subnet_2_id
+
+  vpc_id = module.network.vpc_id
+}
